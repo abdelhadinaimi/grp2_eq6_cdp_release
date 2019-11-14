0a50834eb39ca57aeb60fea0bf41271acd70dbe0
WhizzMirray <naimi.abdelhadi.97@gmail.com>
2019-11-14 19:09:08 +0100

    T_TSK_Mod -> DONE

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Issue = require("./issue.model");
+const Task = require("./task.model");
 const User = require("./user.model");
 
 const projectSchema = new Schema(
@@ -23,6 +24,7 @@ const projectSchema = new Schema(
       required: true
     },
     issues: [Issue.schema],
+    tasks: [Task.schema],
     collaborators: [
       {
         _id: { type: Schema.Types.ObjectId, ref: User.name },
